48bcd703edbbd7a3d49f43017d71279dc50f9a5f
liuxin04 <2597338006@qq.com>
2020-10-11 22:06:43 +0800

    'new1Branch'

--- drag.js
+++ drag.js
@@ -2,5 +2,8 @@ function fn (id) {
   var obj = document.getElementById(id);
   var disX = 0;
   var disY = 0;
-  obj.onmousedown = function () { }
+  obj.onmousedown = function (ev) {
+    disX = ev.pageX - obj.offsetLeft
+    disY = ev.pageY - obj.offsetTop
+  }
 }
