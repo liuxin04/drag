d008163baab8d08f8e07421ffdaa3080f925d2f7
liuxin04 <2597338006@qq.com>
2020-10-12 20:54:26 +0800

    'fix:main/branch'

--- drag.js
+++ drag.js
@@ -5,5 +5,10 @@ function fn (id) {
   obj.onmousedown = function (ev) {
     disX = ev.pageX - obj.offsetLeft
     disY = ev.pageY - obj.offsetTop
+    document.onmousemove = function (ev) {
+      obj.style.left = ev.pageX - disX + 'px';
+      obj.style.top = ev.pageY - disY + 'px'
+    }
+
   }
 }
